0349de1401c4bcdbe9130ab48b6700add1dd7a1b
Lyve <lyve2909+github@gmail.com>
2021-03-26 09:32:26 +0100

    add logging to file support

--- source/dsp56kEmu/logging.cpp
+++ source/dsp56kEmu/logging.cpp
@@ -9,9 +9,42 @@
 #define NOMINMAX
 #include <windows.h>
 
-void g_logWin32( const std::string& _s )
-{
-	::OutputDebugStringA( (_s + "\n").c_str() );
-}
+#include <time.h>
 
+namespace Logging
+{
+	void g_logWin32( const std::string& _s )
+	{
+		::OutputDebugStringA( (_s + "\n").c_str() );
+	}
+
+	std::string buildOutfilename()
+	{
+		char strTime[128];
+
+		time_t t;
+		time(&t);
+
+		tm lt;
+		localtime_s(&lt,&t);
+		strftime( strTime, 127, "%Y-%m-%d-%H-%M-%S", &lt );
+
+		return std::string(strTime) + ".log";
+	}
+
+	static std::string g_outfilename = buildOutfilename();
+
+	void g_logfWin32( const std::string& _s )
+	{
+		FILE* hFile = 0;
+		fopen_s( &hFile, g_outfilename.c_str(), "a" );
+
+		if( hFile )
+		{
+			fputs(_s.c_str(),hFile);
+			fputc('\n',hFile);
+			fclose( hFile );
+		}
+	}
+}
 #endif
--- source/dsp56kEmu/logging.h
+++ source/dsp56kEmu/logging.h
@@ -3,13 +3,19 @@
 #include <sstream>
 #include <string>
 
+
 #ifdef _WIN32
-
+namespace Logging
+{
 	void g_logWin32( const std::string& _s );
+	void g_logfWin32( const std::string& _s );
+}
 
-	#define LOGWIN32(ss)	{ g_logWin32( (ss).str() ); }
+	#define LOGWIN32(ss)	{ Logging::g_logWin32( (ss).str() ); }
+	#define LOGFWIN32(ss)	{ Logging::g_logfWin32( (ss).str() ); }
 #else
 	#define LOGWIN32(S)		{}
+	#define LOGFWIN32(S)	{}
 #endif
 
 //#ifndef _MASTER_BUILD
@@ -18,6 +24,12 @@
 		std::stringstream ss;	ss << __FUNCTION__ << "@" << __LINE__ << ": " << S;												\
 																																\
 		LOGWIN32(ss)																											\
+	}
+	#define LOGF(S)																												\
+	{																															\
+		std::stringstream ss;	ss << S;																						\
+																																\
+		LOGFWIN32(ss)																											\
 	}
 //#else
 //	#define LOG(S)	{}
