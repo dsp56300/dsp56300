db6eada4ebe1e9541f120b3c92bd6ea946ff1970
Lyve <lyve2909+github@gmail.com>
2021-03-27 16:14:55 +0100

    old stuff removed

--- source/dsp56kEmu/essi.cpp
+++ source/dsp56kEmu/essi.cpp
@@ -2,9 +2,6 @@
 
 #include "essi.h"
 
-#include "memory.h"
-#include "utils.h"
-
 namespace dsp56k
 {
 	// _____________________________________________________________________________
@@ -13,21 +10,5 @@ namespace dsp56k
 	void Essi::exec()
 	{
 		return;
-
-		// i don't know what it does but my code is waiting for a change of this bit...
-
-		TWord val = m_memory.get( MemArea_X, 0xffffb3 );
-
-		if( m_toggleState )
-		{
-			bittestandset( val, 0xd );
-		}
-		else
-		{
-			bittestandclear( val, 0xd );
-		}
-		m_toggleState = !m_toggleState;
-
-		m_memory.set( MemArea_X, 0xffffb3, val );
 	}
 };
