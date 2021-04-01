a2cbe52e3396d51981fe7a4cebfd47209dff2bc7
Lyve <lyve2909+github@gmail.com>
2021-04-01 13:35:02 +0200

    remove old microsoft specific pragma

--- source/dsp56kEmu/dsp.cpp
+++ source/dsp56kEmu/dsp.cpp
@@ -22,10 +22,6 @@
 
 //
 
-#ifdef _MSC_VER
-#pragma warning(disable:4355)	// warning C4355: 'this' : used in base member initializer list
-#endif
-
 namespace dsp56k
 {
 	static bool g_dumpPC = true;
