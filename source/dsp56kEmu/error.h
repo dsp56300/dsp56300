369ed8d10c53a89eaacc2a72e68c86f2b7f2a183
Lyve <lyve2909+github@gmail.com>
2021-04-09 12:52:28 +0200

    use new HEX macro in error.h

--- source/dsp56kEmu/error.h
+++ source/dsp56kEmu/error.h
@@ -18,7 +18,7 @@ namespace dsp56k
 #define LOG_ERR_NOTIMPLEMENTED(T)					LOG_ERR_BASE( false, "Not implemented: " << T )
 
 // memory errors
-#define LOG_ERR_MEM_WRITE(ADDR)						LOG_ERR_BASE( true, "Memory Write: " << std::hex << ADDR )
-#define LOG_ERR_MEM_READ(ADDR)						LOG_ERR_BASE( true, "Memory Read: " << std::hex << ADDR )
-#define LOG_ERR_MEM_READ_UNINITIALIZED(AREA,ADDR)	LOG_ERR_BASE( true, "Read uninitialized memory: area " << AREA << " @" << std::hex << ADDR )
-#define LOG_ERR_DSP_ILLEGALINSTRUCTION(OP)			LOG_ERR_BASE( false, "Illegal instruction: " << std::hex << OP )
+#define LOG_ERR_MEM_WRITE(ADDR)						LOG_ERR_BASE( true, "Memory Write: " << HEX(ADDR) )
+#define LOG_ERR_MEM_READ(ADDR)						LOG_ERR_BASE( true, "Memory Read: " << HEX(ADDR) )
+#define LOG_ERR_MEM_READ_UNINITIALIZED(AREA,ADDR)	LOG_ERR_BASE( true, "Read uninitialized memory: area " << AREA << " @" << HEX(ADDR) )
+#define LOG_ERR_DSP_ILLEGALINSTRUCTION(OP)			LOG_ERR_BASE( false, "Illegal instruction: " << HEX(OP) )
