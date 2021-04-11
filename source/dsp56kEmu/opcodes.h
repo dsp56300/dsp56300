99c85d6005ed70b3309b49fb96921d2fadae7b1c
Lyve <lyve2909+github@gmail.com>
2021-04-11 15:16:46 +0200

    fix opcode match collision between CMPM S1S2 vs ROL

--- source/dsp56kEmu/opcodes.h
+++ source/dsp56kEmu/opcodes.h
@@ -113,11 +113,13 @@ namespace dsp56k
 			}
 			else
 			{
-				if(_oi.m_instruction == Tfr)
+				switch (_oi.m_instruction)
 				{
+				case Tfr:
+				case Cmpm_S1S2:
 					// There are two variants for JJJ, one does not allow 001, 010 and 011 (56 bit regs), only 24 bit are allowed.
 					if(v > 0 && v < 4)
-						return false;					
+						return false;
 				}
 			}
 		}
