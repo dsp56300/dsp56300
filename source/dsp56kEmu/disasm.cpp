25b4bda52c2484023144e34873787228dd0cc754
Lyve <lyve2909+github@gmail.com>
2021-04-14 23:28:41 +0200

    disasm: lots of new instructions supported

--- source/dsp56kEmu/disasm.cpp
+++ source/dsp56kEmu/disasm.cpp
@@ -159,6 +159,11 @@ namespace dsp56k
 		return std::string(">*") + (a > 0 ? "+" : "-") + hex(std::abs(_data));
 	}
 
+	std::string absAddr(int _data)
+	{
+		return std::string(">") + hex(_data);
+	}
+
 	const char* aluD(bool ab)
 	{
 		return ab ? "b" : "a";
@@ -226,6 +231,8 @@ namespace dsp56k
 
 	const char* memArea(TWord _area)
 	{
+		if(_area > 1)
+			return "";
 		return _area ? "y:" : "x:";
 	}
 
@@ -267,10 +274,10 @@ namespace dsp56k
 
 		char temp[32];
 	
-		switch ((mmmrrr & 38) >> 3)
+		switch ((mmmrrr & 0x38))
 		{
 		case 0x30:	// absolute address
-			return area + immediateLong(opB);
+			return absAddr(opB);
 		case 0x34:	// immediate data
 			return area + immediateLong(opB);
 		default:
@@ -316,6 +323,22 @@ namespace dsp56k
 		case 7:		return "y1";
 		}
 		return nullptr;
+	}
+
+	const char* decode_alu_GGG( TWord _ggg, bool D )
+	{
+		switch( _ggg )
+		{
+		case 0:		return aluD(!D);
+		case 1:		return "?";
+		case 2:		return "?";
+		case 3:		return "?";
+		case 4:		return "x0";
+		case 5:		return "y0";
+		case 6:		return "x1";
+		case 7:		return "y1";
+		}
+		return "?";
 	}
 
 	std::string decode_RRR( TWord _r )
@@ -394,12 +417,56 @@ namespace dsp56k
 		return 1;
 	}
 
+	const char* decode_JJ_read( TWord jj )
+	{
+		switch( jj )
+		{
+		case 0: return "x0";
+		case 1: return "y0";
+		case 2: return "x1";
+		case 3: return "y1";
+		}
+		return "?";
+	}
+
+	const char* decode_QQQQ_read( TWord _qqqq )
+	{
+		switch( _qqqq )
+		{
+		case 0:  return "x0,x0";
+		case 1:  return "y0,y0";
+		case 2:  return "x1,x0";
+		case 3:  return "y1,y0";
+		case 4:  return "x0,y1";
+		case 5:  return "y0,x0";
+		case 6:  return "x1,y0";
+		case 7:  return "y1,x1";
+		case 8:  return "x1,x1";
+		case 9:  return "y1,y1";
+		case 10: return "x0,x1";
+		case 11: return "y0,y1";
+		case 12: return "y1,x0";
+		case 13: return "x0,y0";
+		case 14: return "y0,x1";
+		case 15: return "y1,y1";
+		}
+		return "?,?";
+	}
+
 	int Disassembler::disassemble(const OpcodeInfo& oi, TWord op, TWord opB)
 	{
 		const auto inst = oi.m_instruction;
 
 		switch (inst)
 		{
+		// ALU operations without any other variables
+		case Dec:
+		case Inc:
+			{
+				const auto d = getFieldValue(inst, Field_d, op);				
+				m_ss << aluD(d);
+			}
+			return 1;			
 		// immediate operand with destination ALU register
 		case Add_xx:
 		case And_xx:
@@ -522,7 +589,17 @@ namespace dsp56k
 				const auto b	= getFieldValue(inst, Field_bbbbb, op);
 				const auto q	= getFieldValue(inst, Field_qqqqqq, op);
 				const auto S	= getFieldValue(inst, Field_S, op);
-				m_ss << immediate(b) << ',' << peripheralP(S, q) << ',' << relativeAddr(opB);
+				m_ss << immediate(b) << ',' << peripheralQ(S, q) << ',' << relativeAddr(opB);
+				return 2;
+			}
+		case Brclr_S:
+		case Brset_S:
+		case Bsclr_S:
+		case Bsset_S:
+			{
+				const auto b	= getFieldValue(inst, Field_bbbbb, op);
+				const auto d	= getFieldValue(inst, Field_DDDDDD, op);
+				m_ss << immediate(b) << ',' << decode_DDDDDD(d) << ',' << relativeAddr(opB);
 				return 2;
 			}
 		case Andi:
@@ -543,6 +620,7 @@ namespace dsp56k
 			return 1;
 		case Asl_S1S2D:
 		case Asr_S1S2D:
+		case Insert_S1S2:
 			{
 				const auto sss = getFieldValue(inst, Field_sss, op);
 				const auto s = getFieldValue(inst, Field_S, op);
@@ -551,96 +629,227 @@ namespace dsp56k
 			}
 			return 1;
 		case Bcc_xxxx:
+		case BScc_xxxx:
 			{
 				const auto cccc = getFieldValue(inst, Field_CCCC, op);
 				m_ss << condition(cccc) << ' ' << relativeAddr(opB);
 			}
-			break;
+			return 2;
 		case Bcc_xxx: 
+		case BScc_xxx:
 			{
 				const auto cccc = getFieldValue(inst, Field_CCCC, op);
 				const auto a = signextend<int,9>(getFieldValue(inst, Field_aaaa, Field_aaaaa, op));
 				m_ss << condition(cccc) << ' ' << relativeAddr(a);
 			}
-			break;
+			return 1;
 		case Bcc_Rn: 
+		case BScc_Rn:
 			{
 				const auto cccc = getFieldValue(inst, Field_CCCC, op);
 				const auto r = getFieldValue(inst, Field_RRR, op);
 				m_ss << condition(cccc) << ' ' << decode_RRR(r);
 			}
-			break;
+			return 1;
 		case Bra_xxxx: 
-			break;
-		case Bra_xxx: break;
-		case Bra_Rn: break;
-		case Brclr_S: break;
-		case BRKcc: break;
-		case Brset_S: break;
-		case BScc_xxxx: break;
-		case BScc_xxx: break;
-		case BScc_Rn: break;
-		case Bsclr_S: break;
-		case Bsr_xxxx: break;
-		case Bsr_xxx: break;
-		case Bsr_Rn: break;
-		case Bsset_S: break;
-		case Clb: break;
-		case Cmpu_S1S2: break;
-		case Debug: break;
-		case Debugcc: break;
-		case Dec: break;
-		case Div: break;
-		case Dmac: break;
-		case Do_ea: break;
-		case Do_aa: break;
-		case Do_xxx: break;
-		case Do_S: break;
-		case DoForever: break;
-		case Dor_ea: break;
-		case Dor_aa: break;
-		case Dor_xxx: break;
-		case Dor_S: break;
-		case DorForever: break;
-		case Enddo: break;
-		case Extract_S1S2: break;
-		case Extract_CoS2: break;
-		case Extractu_S1S2: break;
-		case Extractu_CoS2: break;
-		case Ifcc: break;
-		case Ifcc_U: break;
-		case Illegal: break;
-		case Inc: break;
-		case Insert_S1S2: break;
-		case Insert_CoS2: break;
-		case Jcc_xxx: break;
-		case Jcc_ea: break;
-		case Jclr_ea: break;
-		case Jclr_aa: break;
-		case Jclr_pp: break;
-		case Jclr_qq: break;
-		case Jclr_S: break;
-		case Jmp_ea: break;
-		case Jmp_xxx: break;
-		case Jscc_xxx: break;
-		case Jscc_ea: break;
-		case Jsclr_ea: break;
-		case Jsclr_aa: break;
-		case Jsclr_pp: break;
-		case Jsclr_qq: break;
-		case Jsclr_S: break;
-		case Jset_ea: break;
-		case Jset_aa: break;
-		case Jset_pp: break;
-		case Jset_qq: break;
-		case Jset_S: break;
-		case Jsr_ea: break;
-		case Jsr_xxx: break;
-		case Jsset_ea: break;
-		case Jsset_aa: break;
-		case Jsset_pp: break;
-		case Jsset_qq: break;
-		case Jsset_S: break;
+		case Bsr_xxxx:
+			m_ss << relativeAddr(opB);
+			return 2;
+		case Bra_xxx: 
+		case Bsr_xxx:
+			{
+				const auto a = signextend<int,9>(getFieldValue(inst, Field_aaaa, Field_aaaaa, op));
+				m_ss << relativeAddr(a);
+			}
+			return 1;
+		case Bra_Rn: 
+		case Bsr_Rn:
+			{
+				const auto r = getFieldValue(inst, Field_RRR, op);
+				m_ss << decode_RRR(r);
+			}
+			return 1;
+		case BRKcc:
+		case Debugcc:
+			{
+				const auto cccc = getFieldValue(inst, Field_CCCC, op);
+				m_ss << condition(cccc);
+			}
+			return 1;
+		case Clb:
+			{
+				const auto s = getFieldValue(inst, Field_S, op);
+				const auto d = getFieldValue(inst, Field_D, op);
+				m_ss << aluD(s) << ',' << aluD(d);
+			}
+			return 1;
+		case Cmpu_S1S2:
+			{
+				const auto d = getFieldValue(inst, Field_d, op);
+				const auto ggg = getFieldValue(inst, Field_ggg, op);
+				m_ss << decode_alu_GGG(ggg, d) << ',' << aluD(d);
+			}
+			return 1;
+		case Div:
+			{
+				const auto d = getFieldValue(inst, Field_d, op);
+				const auto jj = getFieldValue(inst, Field_JJ, op);
+				m_ss << decode_JJ_read(jj) << ',' << aluD(d);
+			}
+			return 1;
+		case Dmac:
+			{
+				const bool srcUnsigned	= getFieldValue<Dmac,Field_S>(op);
+				const bool dstUnsigned	= getFieldValue<Dmac,Field_s>(op);
+				const bool ab			= getFieldValue<Dmac,Field_d>(op);
+				const bool negate		= getFieldValue<Dmac,Field_k>(op);
+
+				const TWord qqqq		= getFieldValue<Dmac,Field_QQQQ>(op);
+
+				m_ss << (srcUnsigned ? 'u' : 's');
+				m_ss << (dstUnsigned ? 'u' : 's');
+				m_ss << ' ';
+				if(negate)
+					m_ss << '-';
+				m_ss << decode_QQQQ_read(qqqq) << ',' << aluD(ab);
+			}
+			return 1;
+		case Do_ea: 
+		case Dor_ea:
+			{
+				const auto mr	= getFieldValue(inst, Field_MMM, Field_RRR, op);
+				const auto S	= getFieldValue(inst, Field_S, op);
+				m_ss << mmmrrr(mr, S, opB) << ',' << relativeAddr(opB + 1);
+			}
+			return 2;
+		case Do_aa: 
+		case Dor_aa:
+			{
+				const auto a	= getFieldValue(inst, Field_aaaaaa, op);
+				const auto S	= getFieldValue(inst, Field_S, op);
+				m_ss << memory(S, a) << ',' << relativeAddr(opB + 1);
+			}
+			return 1;
+		case Do_xxx: 
+		case Dor_xxx:
+			{
+				const auto a	= getFieldValue(inst, Field_hhhh, Field_iiiiiiii, op);
+				m_ss << immediate(a) << ',' << relativeAddr(opB + 1);
+			}
+			return 1;
+		case Do_S: 
+		case Dor_S:
+			{
+				const auto d	= getFieldValue(inst, Field_DDDDDD, op);
+				m_ss << decode_DDDDDD(d) << ',' << relativeAddr(opB + 1);
+			}
+			return 1;
+		case Extract_S1S2:
+		case Extractu_S1S2:
+			{
+				const auto sss = getFieldValue(inst, Field_SSS, op);
+				const auto s = getFieldValue(inst, Field_s, op);
+				const auto d = getFieldValue(inst, Field_D, op);
+				m_ss << decode_sss(sss) << ',' << aluD(s) << ',' << aluD(d);
+			}
+			return 1;
+		case Extract_CoS2: 
+		case Extractu_CoS2:
+		case Insert_CoS2:
+			{
+				const auto s = getFieldValue(inst, Field_s, op);
+				const auto d = getFieldValue(inst, Field_D, op);
+				m_ss << immediate(opB) << ',' << aluD(s) << ',' << aluD(d);				
+			}
+			return 2;
+		case Ifcc:
+		case Ifcc_U:
+			{				
+				const auto cccc = getFieldValue(inst, Field_CCCC, op);
+				m_ss << condition(cccc);
+			}
+			return 1;
+		case Jcc_xxx: 
+			{
+				const auto cccc = getFieldValue(inst, Field_CCCC, op);
+				const auto a = signextend<int,9>(getFieldValue(inst, Field_aaaa, Field_aaaaa, op));
+				m_ss << condition(cccc) << ' ' << absAddr(a);
+			}
+			return 1;
+		case Jcc_ea: 
+		case Jscc_ea:
+			{
+				const auto cccc = getFieldValue(inst, Field_CCCC, op);
+				const auto dd = getFieldValue(inst, Field_MMM, Field_RRR, op);
+				m_ss << condition(cccc) << ' ' << decode_DDDDDD(dd);
+			}
+			return 1;
+		case Jclr_ea:
+		case Jset_ea:
+		case Jsclr_ea:
+		case Jsset_ea:
+			{
+				const auto b	= getFieldValue(inst, Field_bbbbb, op);
+				const auto mr	= getFieldValue(inst, Field_MMM, Field_RRR, op);
+				m_ss << immediate(b) << ',' << mmmrrr(mr, 2, opB) << ',' << absAddr(opB);
+			}
+			return 2;
+		case Jclr_aa:
+		case Jset_aa:
+		case Jsclr_aa:
+		case Jsset_aa:
+			{
+				const auto b	= getFieldValue(inst, Field_bbbbb, op);
+				const auto a	= getFieldValue(inst, Field_aaaaaa, op);
+				const auto S	= getFieldValue(inst, Field_S, op);
+				m_ss << immediate(b) << ',' << memory(S, a) << ',' << absAddr(opB);
+			}
+			return 2;
+		case Jclr_pp:
+		case Jsclr_pp:
+		case Jset_pp:
+		case Jsset_pp:
+			{
+				const auto b	= getFieldValue(inst, Field_bbbbb, op);
+				const auto p	= getFieldValue(inst, Field_pppppp, op);
+				const auto S	= getFieldValue(inst, Field_S, op);
+				m_ss << immediate(b) << ',' << peripheralP(S, p) << ',' << absAddr(opB);
+			}
+			return 2;
+		case Jclr_qq:
+		case Jsclr_qq:
+		case Jset_qq:
+		case Jsset_qq:
+			{
+				const auto b	= getFieldValue(inst, Field_bbbbb, op);
+				const auto q	= getFieldValue(inst, Field_qqqqqq, op);
+				const auto S	= getFieldValue(inst, Field_S, op);
+				m_ss << immediate(b) << ',' << peripheralQ(S, q) << ',' << absAddr(opB);
+			}
+			return 2;
+		case Jclr_S:
+		case Jsclr_S:
+		case Jset_S:
+		case Jsset_S:
+			{
+				const auto b	= getFieldValue(inst, Field_bbbbb, op);
+				const auto d	= getFieldValue(inst, Field_DDDDDD, op);
+				m_ss << immediate(b) << ',' << decode_DDDDDD(d) << ',' << absAddr(opB);
+			}
+			return 2;
+		case Jmp_ea: 
+		case Jsr_ea:
+			{
+				const auto mr = getFieldValue(inst, Field_MMM, Field_RRR, op);
+				m_ss << mmmrrr(mr, 2, opB);
+			}
+			return 2;
+
+		case Jmp_xxx:
+		case Jsr_xxx:
+			m_ss << absAddr(opB);
+			return 1;
+
 		case Lra_Rn: break;
 		case Lra_xxxx: break;
 		case Lsl_ii: break;
@@ -695,36 +904,45 @@ namespace dsp56k
 		case Mpyr_S1S2D: break;
 		case Mpyr_SD: break;
 		case Mpyri: break;
-		case Nop: break;
 		case Norm: break;
 		case Normf: break;
 		case Not: break;
 		case Ori: break;
-		case Pflush: break;
-		case Pflushun: break;
-		case Pfree: break;
 		case Plock: break;
-		case Plockr: break;
 		case Punlock: break;
-		case Punlockr: break;
 		case Rep_ea: break;
 		case Rep_aa: break;
 		case Rep_xxx: break;
 		case Rep_S: break;
-		case Reset: break;
-		case Rti: break;
-		case Rts: break;
 		case Sbc: break;
 		case Tcc_S1D1: break;
 		case Tcc_S1D1S2D2: break;
 		case Tcc_S2D2: break;
-		case Trap: break;
 		case Trapcc: break;
 		case Vsl: break;
-		case Wait: break;
 		case ResolveCache: break;
-		case Parallel: break;
-		case InstructionCount: break;
+
+		// Opcodes without parameters
+		case Debug: 
+		case DoForever:
+		case DorForever:
+		case Enddo:
+		case Illegal:
+		case Nop:
+		case Pflush:
+		case Pflushun:
+		case Pfree:
+		case Plockr:
+		case Punlockr:
+		case Reset:
+		case Rti:
+		case Rts:
+		case Stop:
+		case Trap:
+		case Wait:
+			return 1;
+		case Parallel:
+		case InstructionCount:
 		default: 
 			return 0;
 		}
