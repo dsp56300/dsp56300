1cdb57b26d6dd4ff68bfe82738500e0ff13c9e34
Lyve <lyve2909+github@gmail.com>
2021-04-19 17:43:37 +0200

    fix REP

--- source/dsp56kEmu/dsp.cpp
+++ source/dsp56kEmu/dsp.cpp
@@ -1205,14 +1205,13 @@ namespace dsp56k
 
 	bool DSP::rep_exec(const TWord loopCount)
 	{
+		const auto lcBackup = reg.lc;
 		reg.lc.var = loopCount;
 
 		traceOp();
 
 		pcCurrentInstruction = reg.pc.var;
 		const auto op = fetchPC();
-
-		const auto lcBackup = reg.lc;
 
 		execOp(op);
 		// TODO: remember function pointer and call directly
