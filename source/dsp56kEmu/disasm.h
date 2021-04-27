7045d6b5cafba8730becae3918d63b99ea8d13ea
Lyve <lyve2909+github@gmail.com>
2021-04-27 23:31:54 +0200

    only include motorola disassembler if it exists on disk

--- source/dsp56kEmu/disasm.h
+++ source/dsp56kEmu/disasm.h
@@ -1,7 +1,7 @@
 #pragma once
 
-#if defined(_WIN32) && defined(_DEBUG)
-#	define USE_MOTOROLA_UNASM
+#if defined(DSP56K_USE_MOTOROLA_UNASM) && (!defined(_WIN32) || !defined(_DEBUG) || defined(_WIN64))
+#	undef USE_MOTOROLA_UNASM
 #endif
 
 #include <array>
