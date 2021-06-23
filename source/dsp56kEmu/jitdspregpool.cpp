d1038329ef035bd59508daa95a66957c7f2d066d
Lyve <lyve2909+github@gmail.com>
2021-06-24 00:28:40 +0200

    use non-volatile xmm registers first

--- source/dsp56kEmu/jitdspregpool.cpp
+++ source/dsp56kEmu/jitdspregpool.cpp
@@ -10,9 +10,8 @@ namespace dsp56k
 {
 	static constexpr JitReg g_gps[] =	{ asmjit::x86::r8, asmjit::x86::r9, asmjit::x86::r10, asmjit::x86::r11};
 
-	static constexpr JitReg128 g_xmms[] =	{ asmjit::x86::xmm0, asmjit::x86::xmm1, asmjit::x86::xmm2, asmjit::x86::xmm3
-											, asmjit::x86::xmm4, asmjit::x86::xmm5, asmjit::x86::xmm6, asmjit::x86::xmm7
-											, asmjit::x86::xmm8, asmjit::x86::xmm9, asmjit::x86::xmm10, asmjit::x86::xmm11};
+	static constexpr JitReg128 g_xmms[] =	{ asmjit::x86::xmm6, asmjit::x86::xmm7, asmjit::x86::xmm8, asmjit::x86::xmm9, asmjit::x86::xmm10, asmjit::x86::xmm11
+											, asmjit::x86::xmm0, asmjit::x86::xmm1, asmjit::x86::xmm2, asmjit::x86::xmm3, asmjit::x86::xmm4, asmjit::x86::xmm5};
 
 	static constexpr uint32_t g_gpCount = sizeof(g_gps) / sizeof(g_gps[0]);
 	static constexpr uint32_t g_xmmCount = sizeof(g_xmms) / sizeof(g_xmms[0]);
