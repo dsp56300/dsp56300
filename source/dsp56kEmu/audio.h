fcf3065695c9ed12ecd890371275e412ea70e96c
Lyve <lyve2909+github@gmail.com>
2021-07-09 23:08:44 +0200

    increase ring buffer size to 8k

--- source/dsp56kEmu/audio.h
+++ source/dsp56kEmu/audio.h
@@ -204,8 +204,8 @@ namespace dsp56k
 			FrameSyncChannelRight = 0
 		};
 
-		std::array<RingBuffer<uint32_t, 4096, true>, 1> m_audioInputs;
-		std::array<RingBuffer<uint32_t, 4096, true>, 3> m_audioOutputs;
+		std::array<RingBuffer<uint32_t, 8192, true>, 1> m_audioInputs;
+		std::array<RingBuffer<uint32_t, 8192, true>, 3> m_audioOutputs;
 		std::atomic<uint32_t> m_pendingRXInterrupts;
 
 		uint32_t m_frameSyncDSPStatus = FrameSyncChannelLeft;
