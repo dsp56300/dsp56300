0e926a71cbc3f26483c0fa90b40fc3156c31663c
Lyve <lyve2909+github@gmail.com>
2021-10-01 21:50:58 +0200

    test runner executes either JIT tests or interpreter tests depending on build configuration

--- source/dsp56kEmu/dspconfig.h
+++ source/dsp56kEmu/dspconfig.h
@@ -4,12 +4,6 @@
 
 namespace dsp56k
 {
-#ifdef DSP56K_56303
-	using Peripheral = Peripherals56303;
-#else
-	using Peripheral = Peripherals56362;
-#endif
-
 #ifdef DSP56K_AAR_TRANSLATE
 	constexpr bool g_useAARTranslate = true;
 #else
