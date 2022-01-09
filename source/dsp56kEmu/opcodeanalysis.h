8aa104cc0ce255a8095569945c9f3e7307762d77
Lyve <lyve2909+github@gmail.com>
2022-01-09 21:21:00 +0100

    continue with static opcode analysis helpers

--- source/dsp56kEmu/opcodeanalysis.h
+++ source/dsp56kEmu/opcodeanalysis.h
@@ -10,11 +10,11 @@ namespace dsp56k
 {
 	enum class Register : uint64_t
 	{
-		Invalid,
+		Invalid = static_cast<uint64_t>(-1),
 
 		// individual registers
 
-		A0, A1, A2,
+		A0 = 0, A1, A2,
 		B0, B1, B2,
 
 		X0, X1,
@@ -38,12 +38,16 @@ namespace dsp56k
 		Y0X0, Y0X1, Y1X0, Y1X1,
 
 		SR, OMR,
+
+		Count
 	};
 
 	static_assert(static_cast<uint32_t>(Register::COM) < 64);
 
 	enum class RegisterMask : uint64_t
 	{
+		None = 0,
+
 		A0 = (1ull << static_cast<uint64_t>(Register::A0)),
 		A1 = (1ull << static_cast<uint64_t>(Register::A1)),
 		A2 = (1ull << static_cast<uint64_t>(Register::A2)),
@@ -120,6 +124,12 @@ namespace dsp56k
 		SR = MR | CCR,
 		OMR = EOM | COM,
 	};
+
+	static void add(RegisterMask& _a, const RegisterMask _b)
+	{
+		auto a = static_cast<uint64_t>(_a) | static_cast<uint64_t>(_b);
+		_a = static_cast<RegisterMask>(a);
+	}
 
 	constexpr RegisterMask convert(const Register _reg)
 	{
@@ -436,85 +446,88 @@ namespace dsp56k
 		}
 	}
 
-	constexpr Register getRegister(Instruction _inst, Field _field, TWord op)
+	inline Register getRegister(const Instruction _inst, const Field _field, const TWord _op)
 	{
+		if (!hasField(_inst, _field))
+			return Register::Invalid;
+
 		switch (_field)
 		{
 		case Field_CCCC: 
 			return Register::CCR;
 		case Field_d:
 		case Field_D:
-			return getFieldValue(_inst, _field, op) ? Register::B : Register::A;
+			return getFieldValue(_inst, _field, _op) ? Register::B : Register::A;
 		case Field_dd:
 		case Field_ddd:
-			return getRegisterDDDDDD(getFieldValue(_inst, Field_dd, Field_ddd, op));
+			return getRegisterDDDDDD(getFieldValue(_inst, Field_dd, Field_ddd, _op));
 		case Field_dddd:
-			return getRegister_DDDD(getFieldValue(_inst, _field, op));
+			return getRegister_DDDD(getFieldValue(_inst, _field, _op));
 		case Field_ddddd:
 		case Field_dddddd:
 		case Field_DDDD:
 		case Field_DDDDDD:
 		case Field_eeeee:
 		case Field_eeeeee:
-			return getRegisterDDDDDD(getFieldValue(_inst, _field, op));
+			return getRegisterDDDDDD(getFieldValue(_inst, _field, _op));
 		case Field_DDDDD:
-			return getRegister_ddddd_pcr(getFieldValue(_inst, _field, op));
+			return getRegister_ddddd_pcr(getFieldValue(_inst, _field, _op));
 		case Field_e: 
-			return getFieldValue(_inst, _field, op) ? Register::X1 : Register::X0;
+			return getFieldValue(_inst, _field, _op) ? Register::X1 : Register::X0;
 		case Field_ee:
-			return getRegister_ee(getFieldValue(_inst, _field, op));
+			return getRegister_ee(getFieldValue(_inst, _field, _op));
 		case Field_EE:
-			return getRegister_EE(getFieldValue(_inst, _field, op));
+			return getRegister_EE(getFieldValue(_inst, _field, _op));
 		case Field_ff:
 			switch (_inst)
 			{
-			case Movexr_ea:	return getRegister_ee(getFieldValue(_inst, _field, op));
+			case Movexr_ea:	return getRegister_ee(getFieldValue(_inst, _field, _op));
 			case Moveyr_ea:
-			case Movexy:	return getRegister_ff(getFieldValue(_inst, _field, op));
+			case Movexy:	return getRegister_ff(getFieldValue(_inst, _field, _op));
 			default:		return Register::Invalid;
 			}
 		case Field_F: 
-			return getFieldValue(_inst, _field, op) ? Register::Y1 : Register::Y0;
+			return getFieldValue(_inst, _field, _op) ? Register::Y1 : Register::Y0;
 		case Field_ggg:
-			return getRegister_GGG(getFieldValue(_inst, _field, op), getFieldValue(_inst, Field_d, op));
+			return getRegister_GGG(getFieldValue(_inst, _field, _op), getFieldValue(_inst, Field_d, _op));
 		case Field_J:
-			return getFieldValue(_inst, _field, op) ? Register::Y : Register::X;
+			return getFieldValue(_inst, _field, _op) ? Register::Y : Register::X;
 		case Field_JJ:
-			return getRegister_JJ(getFieldValue(_inst, _field, op));
+			return getRegister_JJ(getFieldValue(_inst, _field, _op));
 		case Field_JJJ:
-			return getRegister_JJJ(getFieldValue(_inst, _field, op), getFieldValue(_inst, Field_d, op));
+			return getRegister_JJJ(getFieldValue(_inst, _field, _op), getFieldValue(_inst, Field_d, _op));
 		case Field_L:
 		case Field_LL:
-			return getRegister_LLL(getFieldValue(_inst, Field_L, Field_LL, op));
+			return getRegister_LLL(getFieldValue(_inst, Field_L, Field_LL, _op));
 		case Field_qq:
-			return getRegister_qq(getFieldValue(_inst, _field, op));
+			return getRegister_qq(getFieldValue(_inst, _field, _op));
 		case Field_qqq:
-			return getRegister_qqq(getFieldValue(_inst, _field, op));
+			return getRegister_qqq(getFieldValue(_inst, _field, _op));
 		case Field_QQ:
-			return getRegister_QQ(getFieldValue(_inst, _field, op));
+			return getRegister_QQ(getFieldValue(_inst, _field, _op));
 		case Field_QQQ: 
 		case Field_QQQQ:
-			return getRegister_QQQQ(getFieldValue(_inst, _field, op));
+			return getRegister_QQQQ(getFieldValue(_inst, _field, _op));
 		case Field_rr: 
 			{
-				const TWord mrX = getFieldValue(_inst, Field_MM, Field_RRR, op);
-				TWord rrY = getFieldValue(_inst, Field_rr, op);
+				const TWord mrX = getFieldValue(_inst, Field_MM, Field_RRR, _op);
+				TWord rrY = getFieldValue(_inst, Field_rr, _op);
 				rrY += ((mrX & 0x7) >= 4) ? 0 : 4;
 				return getRegister_RRR(rrY);
 			}
 		case Field_RRR: 
-			return getRegister_RRR(getFieldValue(_inst, Field_L, Field_LL, op));
+			return getRegister_RRR(getFieldValue(_inst, Field_L, Field_LL, _op));
 		case Field_s:
 			switch (_inst)
 			{
 			case Extract_CoS2:
 			case Extract_S1S2:
 			case Extractu_CoS2:
-			case Extractu_S1S2:		return getFieldValue(_inst, _field, op) ? Register::B : Register::A;
+			case Extractu_S1S2:		return getFieldValue(_inst, _field, _op) ? Register::B : Register::A;
 			default:				return Register::Invalid;
 			}
 		case Field_sss: 
-			return getRegister_sss(getFieldValue(_inst, _field, op));
+			return getRegister_sss(getFieldValue(_inst, _field, _op));
 		case Field_S: 
 			switch (_inst)
 			{
@@ -523,16 +536,494 @@ namespace dsp56k
 			case Asl_S1S2D:
 			case Asr_S1S2D:
 			case Clb:
-			case Vsl:				return getFieldValue(_inst, _field, op) ? Register::B : Register::A;
+			case Vsl:				return getFieldValue(_inst, _field, _op) ? Register::B : Register::A;
 			default:				return Register::Invalid;
 			}
 		case Field_SSS:
-			return getRegister_sss(getFieldValue(_inst, _field, op));
+			return getRegister_sss(getFieldValue(_inst, _field, _op));
 		case Field_ttt:
 		case Field_TTT:
-			return getRegister_RRR(getFieldValue(_inst, _field, op));
+			return getRegister_RRR(getFieldValue(_inst, _field, _op));
 		default: 
 			return Register::Invalid;
+		}
+	}
+
+	inline RegisterMask getRegisters(const Instruction _inst, const Field _field, const TWord _op)
+	{
+		const RegisterMask mask = convert(getRegister(_inst, _field, _op));
+		return mask;
+	}
+
+	inline void getRegisters(RegisterMask& _written, RegisterMask& _read, const Instruction _inst, const TWord _op)
+	{
+		_read = _written = RegisterMask::None;
+
+		constexpr auto add = [](RegisterMask& _target, Register _src, uint32_t _offset)
+		{
+			const auto r = static_cast<Register>(static_cast<uint32_t>(_src) + _offset);
+			if (r == Register::Invalid)
+				return;
+			dsp56k::add(_target, convert(r));
+		};
+
+		const auto read = [&](const Register _reg, const uint32_t _offset = 0)	{ add(_read, _reg, _offset); };
+		const auto write = [&](const Register _reg, const uint32_t _offset = 0)	{ add(_written, _reg, _offset); };
+
+		const auto readf = [&](const Field _field) { const auto r = getRegister(_inst, _field, _op); read(r); return r; };
+		const auto writef = [&](const Field _field) { const auto r = getRegister(_inst, _field, _op); write(r); return r; };
+		const auto readwritef = [&](const Field _field) { const auto r = getRegister(_inst, _field, _op); read(r); write(r); return r; };
+
+		if (g_opcodes[_inst].flag(OpFlagCondition))
+		{
+			readf(Field_CCCC);
+		}
+
+		if (g_opcodes[_inst].flag(OpFlagBranch))
+		{
+			write(Register::PC);
+		}
+
+		if (g_opcodes[_inst].flag(OpFlagPushPC))
+		{
+			write(Register::SSH);
+			write(Register::SSL);
+		}
+
+		if (g_opcodes[_inst].flag(OpFlagPopPC))
+		{
+			write(Register::PC);
+			read(Register::SSH);
+		}
+
+		if (g_opcodes[_inst].flags(OpFlagRepImmediate, OpFlagRepDynamic))
+		{
+			write(Register::LC);
+			read(Register::LC);
+		}
+		else if(g_opcodes[_inst].flag(OpFlagLoop))
+		{
+			write(Register::LC);
+			write(Register::LA);
+			read(Register::LC);
+			read(Register::LA);
+		}
+
+		if (g_opcodes[_inst].flag(OpFlagCCR))
+			write(Register::CCR);
+
+		if(hasField(_inst, Field_MM) || hasField(_inst, Field_MMM))
+		{
+			const auto mmm = getFieldValue(_inst, hasField(_inst, Field_MM) ? Field_MM : Field_MMM, _op);
+			const auto rrr = getFieldValue(_inst, Field_RRR, _op);
+
+			switch (mmm << 3 | rrr)
+			{
+			case MMMRRR_AbsAddr:
+			case MMMRRR_ImmediateData:
+				break;
+			default:
+				if (mmm == MMM_RnMinusNn || mmm == MMM_RnPlusNn || mmm == MMM_RnPlusNnNoUpdate)
+					read(Register::N0, rrr);
+				if(mmm != MMM_RnPlusNnNoUpdate && mmm != MMM_Rn)
+					write(Register::R0, rrr);
+				if (mmm != MMM_Rn)
+					read(Register::M0, rrr);
+			}
+		}
+
+		switch (_inst)
+		{
+		case Abs:
+			readwritef(Field_d);
+			break;
+		case Add_SD:
+		case Sub_SD:
+			readf(Field_JJJ);
+			readwritef(Field_d);
+			break;
+		case Add_xx:
+		case Add_xxxx:
+		case Sub_xx:
+		case Sub_xxxx:
+		case And_xx:
+		case And_xxxx:
+		case Eor_xx:
+		case Eor_xxxx:
+		case Or_xx:
+		case Or_xxxx:
+			readwritef(Field_d);
+			break;
+		case ADC:
+		case Addl:
+		case Addr:
+		case Subl:
+		case Subr:
+			read(readwritef(Field_d) == Register::A ? Register::B : Register::A);
+			break;
+		case And_SD: 
+		case Or_SD:
+		case Eor_SD:
+			readf(Field_JJ);
+			readwritef(Field_d);
+			break;
+		case Andi:
+		case Ori:
+			readwritef(Field_EE);
+			break;
+		case Asl_ii:
+		case Asr_ii:
+		case Asl_S1S2D:
+		case Asr_S1S2D:
+			readf(Field_S);
+			writef(Field_D);
+			break;
+		case Bcc_xxxx:
+		case Bcc_xxx:
+		case Bra_xxxx:
+		case Bra_xxx:
+		case Bsr_xxxx:
+		case Bsr_xxx:
+		case BScc_xxxx:
+		case BScc_xxx:
+			read(Register::PC);
+			break;
+		case Bcc_Rn:
+		case Bra_Rn:
+		case Bsr_Rn:
+		case BScc_Rn:
+			read(Register::PC);
+			readf(Field_RRR);
+			break;
+		case Bchg_D:
+		case Bclr_D:
+		case Bset_D:
+		case Brclr_S:
+		case Brset_S:
+		case Bsclr_S:
+		case Bsset_S:
+			readwritef(Field_DDDDDD);
+			break;
+		case Btst_D:
+			readf(Field_DDDDDD);
+			break;
+		case Clb:
+			read(readwritef(Field_D) == Register::A ? Register::B : Register::A);
+			break;
+		case Clr:
+			writef(Field_d);
+			break;
+		case Cmp_S1S2:
+		case Cmpm_S1S2:
+			readf(Field_d);
+			readf(Field_JJJ);
+			break;
+		case Cmp_xxS2: 
+		case Cmp_xxxxS2:
+			readf(Field_d);
+			break;
+		case Cmpu_S1S2:
+			readf(Field_ggg);
+			readf(Field_d);
+			break;
+		case Dec:
+			readwritef(Field_d);
+			break;
+		case Div:
+			readf(Field_JJ);
+			readwritef(Field_d);
+			break;
+		case Dmac:
+			readwritef(Field_d);
+			readf(Field_QQQQ);
+			break;
+		case Do_S:
+		case Dor_S:
+			readf(Field_DDDDDD);
+			break;
+		case Extract_S1S2:
+		case Extractu_S1S2:
+		case Extract_CoS2:
+		case Extractu_CoS2:
+		case Insert_S1S2:
+		case Insert_CoS2:
+			writef(Field_D);
+			readf(Field_S);
+			break;
+		case Inc:
+			readwritef(Field_d);
+			break;
+		case Jclr_S:
+		case Jsclr_S:
+		case Jset_S:
+		case Jsset_S:
+			readf(Field_DDDDDD);
+			break;
+		case Lra_Rn:
+			read(Register::PC);
+			writef(Field_ddddd);
+			break;
+		case Lra_xxxx: 
+			read(Register::PC);
+			readf(Field_RRR);
+			writef(Field_ddddd);
+			break;
+		case Asl_D:
+		case Asr_D:
+		case Lsl_D:
+		case Lsr_D:
+		case Lsl_ii:
+		case Lsr_ii:
+			readwritef(Field_D);
+			break;
+		case Lsl_SD:
+		case Lsr_SD:
+			readf(Field_sss);
+			readwritef(Field_D);
+			break;
+		case Lua_ea:
+			writef(Field_ddddd);
+			break;
+		case Lua_Rn:
+			readf(Field_RRR);
+			writef(Field_dddd);
+			break;
+		case Mac_S1S2:
+		case Macr_S1S2:
+			readf(Field_QQQ);
+			readwritef(Field_d);
+			break;
+		case Mac_S:
+		case Macr_S:
+			readf(Field_QQ);
+			readwritef(Field_d);
+			break;
+		case Maci_xxxx:
+		case Macri_xxxx:
+			readf(Field_qq);
+			readwritef(Field_d);
+			break;
+		case Macsu:
+			readf(Field_QQQQ);
+			readwritef(Field_d);
+			break;
+		case Max:
+		case Maxm:
+			read(Register::A);
+			read(Register::B);
+			write(Register::B);
+			break;
+		case Merge:
+			readf(Field_SSS);
+			writef(Field_D);
+			break;
+		case Move_xx:
+			writef(Field_ddddd);
+			break;
+		case Mover: 
+			readf(Field_eeeee);
+			writef(Field_ddddd);
+			break;
+		case Movex_ea:
+		case Movex_aa:
+		case Movey_ea:
+		case Movey_aa:
+			{
+				const auto writeReg = getFieldValue(_inst, Field_W, _op);
+				if (writeReg)
+					writef(Field_ddd);
+				else
+					readf(Field_ddd);
+			}
+			break;
+		case Movex_Rnxxxx: 
+		case Movey_Rnxxxx:
+			{
+				readf(Field_RRR);
+				const auto writeReg = getFieldValue(_inst, Field_W, _op);
+				if (writeReg)
+					writef(Field_DDDDDD);
+				else
+					readf(Field_DDDDDD);
+			}
+			break;
+		case Movex_Rnxxx: 
+		case Movey_Rnxxx:
+			{
+				readf(Field_RRR);
+				const auto writeReg = getFieldValue(_inst, Field_W, _op);
+				if (writeReg)
+					writef(Field_DDDD);
+				else
+					readf(Field_DDDD);
+			}
+			break;
+		case Movexr_ea:
+			{
+				readf(Field_d);
+				writef(Field_F);
+				const auto writeReg = getFieldValue(_inst, Field_W, _op);
+				if (writeReg)
+					writef(Field_ff);
+				else
+					readf(Field_ff);
+			}
+			break;
+		case Movexr_A:
+			readwritef(Field_d);
+			read(Register::X0);
+			break;
+		case Moveyr_ea:
+			{
+				readf(Field_d);
+				writef(Field_e);
+				const auto writeReg = getFieldValue(_inst, Field_W, _op);
+				if (writeReg)
+					writef(Field_ff);
+				else
+					readf(Field_ff);
+			}
+			break;
+		case Moveyr_A:
+			readwritef(Field_d);
+			read(Register::Y0);
+			break;
+		case Movel_ea:
+		case Movel_aa:
+			{
+				const auto writeReg = getFieldValue(_inst, Field_W, _op);
+				if (writeReg)
+					writef(Field_LL);
+				else
+					readf(Field_LL);
+			}
+			break;
+		case Movexy:
+			{
+				const auto writeX = getFieldValue(_inst, Field_W, _op);
+				const auto writeY = getFieldValue(_inst, Field_w, _op);
+
+				if (writeX)	writef(Field_ee);
+				else		readf(Field_ee);
+
+				if (writeY)	writef(Field_ff);
+				else		readf(Field_ff);
+		}
+			break;
+		case Movec_ea:
+		case Movec_aa:
+			{
+				const auto writeReg = getFieldValue(_inst, Field_W, _op);
+				if (writeReg)
+					writef(Field_DDDDD);
+				else
+					readf(Field_DDDDD);
+			}
+			break;
+		case Movec_S1D2:
+			{
+				const auto writeD = getFieldValue(_inst, Field_W, _op);
+				if(writeD)
+				{
+					writef(Field_DDDDD);
+					readf(Field_eeeeee);
+				}
+				else
+				{
+					writef(Field_eeeeee);
+					readf(Field_DDDDD);
+				}
+			}
+			break;
+		case Movec_xx:
+			writef(Field_DDDDD);
+			break;
+		case Movem_ea:
+		case Movem_aa:
+			{
+				const auto writeReg = getFieldValue(_inst, Field_W, _op);
+				if (writeReg)
+					writef(Field_dddddd);
+				else
+					readf(Field_dddddd);
+			}
+			break;
+		case Movep_Spp:
+		case Movep_SXqq:
+		case Movep_SYqq:
+			{
+				const auto writePeriph = getFieldValue(_inst, Field_W, _op);
+				if (writePeriph)
+					readf(Field_dddddd);
+			}
+			break;
+		case Mpy_S1S2D:
+		case Mpyr_S1S2D:
+			writef(Field_d);
+			readf(Field_QQQ);
+			break;
+		case Mpy_SD:
+		case Mpyr_SD:
+			writef(Field_d);
+			readf(Field_QQ);
+			break;
+		case Mpy_su:
+			writef(Field_d);
+			readf(Field_QQQQ);
+			break;
+		case Mpyi:
+		case Mpyri:
+			writef(Field_d);
+			readf(Field_qq);
+			break;
+		case Neg:
+			readwritef(Field_d);
+			break;
+		case Norm:
+			readwritef(Field_d);
+			readf(Field_RRR);
+			break;
+		case Normf:
+			readwritef(Field_d);
+			readf(Field_sss);
+			break;
+		case Not:
+			readwritef(Field_d);
+			break;
+		case Rep_S:
+			readf(Field_dddddd);
+			break;
+		case Rnd:
+		case Rol:
+		case Ror:
+			readwritef(Field_d);
+			break;
+		case Sbc:
+			readf(Field_J);
+			readwritef(Field_d);
+			break;
+		case Tcc_S1D1:
+		case Tfr:
+			readf(Field_JJJ);
+			writef(Field_d);
+			break;
+		case Tcc_S1D1S2D2:
+			readf(Field_ttt);
+			writef(Field_TTT);
+			readf(Field_JJJ);
+			writef(Field_d);
+			break;
+		case Tcc_S2D2:
+			readf(Field_ttt);
+			writef(Field_TTT);
+			break;
+		case Tst:
+			readf(Field_d);
+			break;
+		case Vsl:
+			readf(Field_S);
+			break;
+		default:;
 		}
 	}
 }
