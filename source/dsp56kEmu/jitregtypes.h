135402d3e2a826ddbe1341992312fd8c65e55f1c
Lyve <lyve2909+github@gmail.com>
2022-01-14 21:57:56 +0100

    fix Linux crash if rdi is used as dsp reg ptr

--- source/dsp56kEmu/jitregtypes.h
+++ source/dsp56kEmu/jitregtypes.h
@@ -49,6 +49,7 @@ namespace dsp56k
 
 	static constexpr JitRegGP g_dspPoolGps[] = { asmjit::x86::rdx, asmjit::x86::r8, asmjit::x86::r9, asmjit::x86::r10, asmjit::x86::r11, asmjit::x86::rbx, asmjit::x86::rdi};
 
+	static constexpr auto regDspPtr = asmjit::x86::rsi;
 #else
 	static constexpr JitReg64 g_funcArgGPs[] = { asmjit::x86::rdi, asmjit::x86::rsi, asmjit::x86::rdx, asmjit::x86::rcx };
 
@@ -59,13 +60,12 @@ namespace dsp56k
 	
 	static constexpr JitReg128 g_nonVolatileXMMs[] = { asmjit::x86::xmm6, asmjit::x86::xmm7, asmjit::x86::xmm8, asmjit::x86::xmm9, asmjit::x86::xmm10, asmjit::x86::xmm11, asmjit::x86::xmm12, asmjit::x86::xmm13, asmjit::x86::xmm14, asmjit::x86::xmm15 };
 
-	static constexpr JitRegGP g_dspPoolGps[] = { asmjit::x86::rbx, asmjit::x86::rdx, asmjit::x86::r8, asmjit::x86::r9, asmjit::x86::r10, asmjit::x86::r11};
+	static constexpr JitRegGP g_dspPoolGps[] = { asmjit::x86::rdx, asmjit::x86::r8, asmjit::x86::r9, asmjit::x86::r10, asmjit::x86::r11, asmjit::x86::rsi, asmjit::x86::rdi };
 	
+	static constexpr auto regDspPtr = asmjit::x86::rbx;
 #endif
 
 	static constexpr auto regReturnVal = asmjit::x86::rax;
-
-	static constexpr auto regDspPtr = asmjit::x86::rsi;
 
 	static constexpr std::initializer_list<JitReg> g_regGPTemps = { asmjit::x86::r12, asmjit::x86::r13, asmjit::x86::r14, asmjit::x86::r15, asmjit::x86::rbp };
 
