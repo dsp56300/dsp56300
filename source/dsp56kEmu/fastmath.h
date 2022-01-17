5aca3bc51af3d9d5e543127e940c88b5bc40ed7a
Lyve <lyve2909+github@gmail.com>
2022-01-17 17:47:41 +0100

    remove obsolete math stuff

--- source/dsp56kEmu/fastmath.h
+++ source/dsp56kEmu/fastmath.h
@@ -43,70 +43,6 @@ inline int floor_int(double _value)
 	}
 #endif
 
-	inline float fastexp3(float x) 
-	{ 
-		return (6+x*(6+x*(3+x)))*0.16666666f; 
-	}
-
-	inline float fastexp4(float x) 
-	{
-		return (24+x*(24+x*(12+x*(4+x))))*0.041666666f;
-	}
-
-	inline float fastexp5(float x) 
-	{
-		return (120+x*(120+x*(60+x*(20+x*(5+x)))))*0.0083333333f;
-	}
-
-	inline float fastexp6(float x) 
-	{
-		return 720+x*(720+x*(360+x*(120+x*(30+x*(6+x)))))*0.0013888888f;
-	}
-
-	inline float fastexp7(float x) 
-	{
-		return (5040+x*(5040+x*(2520+x*(840+x*(210+x*(42+x*(7+x)))))))*0.00019841269f;
-	}
-
-	inline float fastexp8(float x) 
-	{
-		return (40320+x*(40320+x*(20160+x*(6720+x*(1680+x*(336+x*(56+x*(8+x))))))))*2.4801587301e-5f;
-	}
-
-	inline float fastexp9(float x) 
-	{
-		return (362880+x*(362880+x*(181440+x*(60480+x*(15120+x*(3024+x*(504+x*(72+x*(9+x)))))))))*2.75573192e-6f;
-	}
-
-	static inline float	fastpow2 (float p)
-	{
-		float offset = (p < 0) ? 1.0f : 0.0f;
-		float clipp = (p < -126) ? -126.0f : p;
-		int w = floor_int(clipp);
-		float z = clipp - w + offset;
-		union { unsigned int i; float f; } v = { static_cast<unsigned int>( (1 << 23) * (clipp + 121.2740575f + 27.7280233f / (4.84252568f - z) - 1.49012907f * z) ) };
-
-		return v.f;
-	}
-
-	static inline float	fastpow2PositiveOnly (float clipp)
-	{
-		int w = floor_int(clipp);
-		float z = clipp - w;
-		union { unsigned int i; float f; } v = { static_cast<unsigned int>( (1 << 23) * (clipp + 121.2740575f + 27.7280233f / (4.84252568f - z) - 1.49012907f * z) ) };
-
-		return v.f;
-	}
-	static inline float fastexp (float p)
-	{
-		return fastpow2 (1.442695040f * p);
-	}
-
-	static inline float fastexpPositiveOnly (float p)
-	{
-		return fastpow2PositiveOnly (1.442695040f * p);
-	}
-
 #ifdef HAVE_SSE
 	inline float min(float a, float b)
 	{
@@ -153,24 +89,4 @@ inline int floor_int(double _value)
 	}
 
 	template<typename T> T lerp(const T& a, const T& b, const T& val) { return a + (b - a) * val; }
-
-	inline void blendControlForTwoParams(float& _dstA, float& _dstB, float _paramMinus1to1)
-	{
-		// _dstA will be:
-		// ______
-		//       \
-		//        \
-		//         \
-		//          \
-
-		// _dstB will be:
-		//     ______
-		//    /
-		//   /
-		//  /
-		// /
-
-		_dstA = min(1.f - _paramMinus1to1, 1.f);
-		_dstB = min(_paramMinus1to1 + 1.f, 1.f);
-	}
 }
