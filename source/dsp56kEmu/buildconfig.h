6e5c3951f103c5235098cda583c46e87997bb0aa
dsp56300 <dsp56300@users.noreply.github.com>
2024-05-09 22:04:33 +0200

    remove _WIN64 as test for X86_X64 as it won't be valid anymore if there is Windows ARM

--- source/dsp56kEmu/buildconfig.h
+++ source/dsp56kEmu/buildconfig.h
@@ -1,13 +1,13 @@
 #pragma once
 
-#if defined(_M_IX86) || defined(_WIN64) || defined(__i386__) || defined(__x86_64__)
+#if defined(_M_IX86) || defined(_M_X64) || defined(__i386__) || defined(__x86_64__)
 #	define HAVE_SSE
 #endif
 
-#if defined(_WIN64) || defined(__x86_64__) || defined(__x86_64) || defined(__amd__64__)
+#if defined(_M_X64) || defined(__x86_64__) || defined(__x86_64) || defined(__amd__64__)
 #	define HAVE_X86_64
 #endif
 
-#if defined(__aarch64__) || defined(__ARM_ARCH_8)
+#if defined(__aarch64__) || defined(__ARM_ARCH_8) || defined(_M_ARM64)
 #	define HAVE_ARM64
 #endif
