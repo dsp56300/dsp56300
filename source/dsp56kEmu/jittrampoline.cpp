b9776c652c527e98bc6133883fa9c81daace89c5
dsp56300 <dsp56300@users.noreply.github.com>
2025-02-13 23:54:10 +0100

    optimize trampoline code

--- source/dsp56kEmu/jittrampoline.cpp
+++ source/dsp56kEmu/jittrampoline.cpp
@@ -9,21 +9,26 @@
 namespace dsp56k
 {
 #ifdef HAVE_ARM64
-	constexpr auto g_funcToCall = JitReg64(21);
 	constexpr auto g_ptrDSP = JitReg64(22);
 	constexpr auto g_counter = JitReg64(23);
-	constexpr auto g_temp = JitReg64(24);
+	constexpr auto g_ptrJitEntries = JitReg64(24);
+	constexpr auto g_ptrPC = JitReg64(25);
+	constexpr auto g_ptrInterruptFunc = JitReg64(26);
 #else
-	constexpr auto g_funcToCall = asmjit::x86::rbp;
 	constexpr auto g_ptrDSP = asmjit::x86::r12;
 	constexpr auto g_counter = asmjit::x86::r13;
-	constexpr auto g_temp = asmjit::x86::r14;
+	constexpr auto g_ptrJitEntries = asmjit::x86::r14;
+	constexpr auto g_ptrPC = asmjit::x86::r15;
+	constexpr auto g_ptrInterruptFunc = asmjit::x86::rbp;
 #endif
 
-	static_assert(!g_funcToCall.equals(regDspPtr));
+	constexpr auto g_funcToCall = g_funcArgGPs[2];
+
+	static_assert(!g_ptrInterruptFunc.equals(regDspPtr));
 	static_assert(!g_ptrDSP.equals(regDspPtr));
 	static_assert(!g_counter.equals(regDspPtr));
-	static_assert(!g_temp.equals(regDspPtr));
+	static_assert(!g_ptrJitEntries.equals(regDspPtr));
+	static_assert(!g_ptrPC.equals(regDspPtr));
 
 	JitTrampoline::JitTrampoline(DSP& _dsp) : m_dsp(_dsp)
 	{
@@ -42,68 +47,72 @@ namespace dsp56k
 		m_asm.addDiagnosticOptions(asmjit::DiagnosticOptions::kValidateAssembler);
 
 		// fill nonvolatile registers with pointers that we need
-		m_asm.push(r64(g_funcToCall));
 		m_asm.push(r64(g_ptrDSP));
 		m_asm.push(r64(g_counter));
-		m_asm.push(r64(g_temp));
 		m_asm.push(r64(regDspPtr));
+		m_asm.push(r64(g_ptrJitEntries));
+		m_asm.push(r64(g_ptrInterruptFunc));
+		m_asm.push(r64(g_ptrPC));
 
 #ifdef HAVE_ARM64
 		m_asm.push(asmjit::a64::regs::x30);
 #endif
 
+#ifdef HAVE_X86_64
 #ifdef _WIN32
-		m_asm.sub(asmjit::x86::regs::rsp, asmjit::Imm(32));	// shadow space
+		static constexpr uint32_t g_additionalStackSize = 32 + 8;	// shadow space + alignment
+#else
+		static constexpr uint32_t g_additionalStackSize = 8;		// alignment
+#endif
+		m_asm.sub(asmjit::x86::regs::rsp, asmjit::Imm(g_additionalStackSize));
 #endif
 
-		m_asm.mov(r64(g_ptrDSP), r64(g_funcArgGPs[0]));
-		m_asm.mov(r32(g_counter), r32(g_funcArgGPs[1]));
+		const auto argDspPtr = r64(g_funcArgGPs[0]);
+		const auto argCounter = r32(g_funcArgGPs[1]);
 
-		const auto ptrDspRegs       = Jitmem::makeRelativePtr(&m_dsp.regs()            , &m_dsp, g_ptrDSP, 8);
-		const auto ptrJitEntries    = Jitmem::makeRelativePtr(&m_dsp.getJitEntries()   , &m_dsp, g_ptrDSP, 8);
-		const auto ptrInterruptFunc = Jitmem::makeRelativePtr(&m_dsp.getInterruptFunc(), &m_dsp, g_ptrDSP, 8);
-		const auto ptrPC            = Jitmem::makeRelativePtr(&m_dsp.regs().pc.var     , &m_dsp, g_ptrDSP, 4);
+		m_asm.mov(r64(g_ptrDSP), argDspPtr);
+		m_asm.mov(r32(g_counter), argCounter);
 
+		m_asm.lea_(g_ptrJitEntries   , argDspPtr, &m_dsp.getJitEntries(), &m_dsp);
+		m_asm.lea_(g_ptrInterruptFunc, argDspPtr, &m_dsp.getInterruptFunc(), &m_dsp);
+		m_asm.lea_(g_ptrPC           , argDspPtr, &m_dsp.regs().pc.var, &m_dsp);
+
+		const auto ptrDspRegs = Jitmem::makeRelativePtr(&m_dsp.regs(), &m_dsp, argDspPtr, 8);
 		assert(ptrDspRegs.offset());
-		assert(ptrJitEntries.offset());
-		assert(ptrInterruptFunc.offset());
-		assert(ptrPC.offset());
 
 #ifdef HAVE_ARM64
 		m_asm.add(regDspPtr, g_ptrDSP, ptrDspRegs.offset());
 #else
 		m_asm.lea(regDspPtr, ptrDspRegs);
 #endif
+
 		const auto label = m_asm.newNamedLabel("beginExec8times");
 		m_asm.bind(label);
 
 		for(uint32_t i=0; i<UnrollSize; ++i)
 		{
-			// 1) call interrupt func first, arg0 = pointer to DSP
-			// 2) call JIT func, arg0 = pointer to Jit, arg1 = PC
+			// 1) call interrupt func: (DSP*)
+			// 2) call JIT func:       (DspRegs*, PC)
 
 #ifdef HAVE_ARM64
-			m_asm.mov(g_temp, ptrInterruptFunc);
+			m_asm.ldr(g_funcToCall, Jitmem::makePtr(g_ptrInterruptFunc, 8));
 			m_asm.mov(g_funcArgGPs[0], g_ptrDSP);
+			m_asm.blr(g_funcToCall);
 
-			m_asm.blr(g_temp);
-
-			m_asm.mov(g_funcToCall, ptrJitEntries);
-			m_asm.mov(r32(g_funcArgGPs[1]), ptrPC);
+			m_asm.ldr(g_funcToCall, Jitmem::makePtr(g_ptrJitEntries, 8));
+			m_asm.ldr(r32(g_funcArgGPs[1]), Jitmem::makePtr(g_ptrPC, 4));
+			m_asm.ldr(g_funcToCall, Jitmem::makePtr(g_funcToCall, g_funcArgGPs[1], 3, 8));
 			m_asm.mov(r64(g_funcArgGPs[0]), regDspPtr);
-			m_asm.ldr(g_funcToCall, Jitmem::makePtr(g_funcToCall, g_funcArgGPs[1], 3));
-
 			m_asm.blr(g_funcToCall);
 #else
-			m_asm.mov(g_temp, ptrInterruptFunc);
+			m_asm.mov(g_funcToCall, Jitmem::makePtr(g_ptrInterruptFunc, 8));
 			m_asm.mov(g_funcArgGPs[0], g_ptrDSP);
-			m_asm.call(g_temp);
+			m_asm.call(g_funcToCall);
 
-			m_asm.mov(g_funcToCall, ptrJitEntries);
-			m_asm.mov(r32(g_funcArgGPs[1]), ptrPC);
-			m_asm.mov(r64(g_funcArgGPs[0]), regDspPtr);
+			m_asm.mov(g_funcToCall, Jitmem::makePtr(g_ptrJitEntries, 8));
+			m_asm.mov(r32(g_funcArgGPs[1]), Jitmem::makePtr(g_ptrPC, 4));
 			m_asm.mov(g_funcToCall, Jitmem::makePtr(g_funcToCall, g_funcArgGPs[1], 3, 8));
-
+			m_asm.mov(r64(g_funcArgGPs[0]), regDspPtr);
 			m_asm.call(g_funcToCall);
 #endif
 		}
@@ -115,19 +124,19 @@ namespace dsp56k
 #endif
 		m_asm.jnz(label);
 
-#ifdef _WIN32
-		m_asm.add(asmjit::x86::regs::rsp, asmjit::Imm(32));	// shadow space
+#ifdef HAVE_X86_64
+		m_asm.add(asmjit::x86::regs::rsp, asmjit::Imm(g_additionalStackSize));
 #endif
 
 #ifdef HAVE_ARM64
 		m_asm.pop(asmjit::a64::regs::x30);
 #endif
-
+		m_asm.pop(r64(g_ptrPC));
+		m_asm.pop(r64(g_ptrInterruptFunc));
+		m_asm.pop(r64(g_ptrJitEntries));
 		m_asm.pop(r64(regDspPtr));
-		m_asm.pop(r64(g_temp));
 		m_asm.pop(r64(g_counter));
 		m_asm.pop(r64(g_ptrDSP));
-		m_asm.pop(r64(g_funcToCall));
 
 		m_asm.ret();
 		m_asm.finalize();
